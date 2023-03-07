53ae28c761167a0e593822bca6fa458c52b1cc81
apm <apm@matlock.ca>
2023-03-07 19:26:07 -0400

    exercise 3 complete

---
+++ 3/main.py
@@ -0,0 +1,26 @@
+def isEven(number):
+    if type(number) == float:
+        return False
+    elif number % 2 == 0:
+        return True
+    else:
+        return False
+
+def isOdd(number):
+    if type(number) == float:
+        return False
+    elif number % 2 != 0:
+        return True
+    else:
+        return False
+    
+assert isOdd(42) == False
+assert isOdd(9999) == True
+assert isOdd(-10) == False
+assert isOdd(-11) == True
+assert isOdd(3.1415) == False
+assert isEven(42) == True
+assert isEven(9999) == False
+assert isEven(-10) == True
+assert isEven(-11) == False
+assert isEven(3.1415) == False
