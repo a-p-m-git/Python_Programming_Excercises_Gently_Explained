a3158316082568ca46c52684c383d1dd44e624f4
apm <github@matlock.ca>
2023-03-09 16:37:34 -0400

    execrise 6 in progr

--- 6 Ordinal Suffix/main.py
+++ 6 Ordinal Suffix/main.py
@@ -10,6 +10,17 @@
 
 
 def ordinalSuffix(number):
+    return number , len(str(number))
+    
+    
+    
+    
+    """ 
+    if len(str(number)) > 2:
+    
+    else:
+    
+    
     
     if len(str(number)) >= 2:
         if str(number)[-2:] == "11" or str(number)[-2:] == "12" or str(number)[-2:] == "13":
@@ -23,29 +34,22 @@ def ordinalSuffix(number):
     elif str(number)[-1] == "3":
         return str(number) + "rd"
     else:
-        return str(number) + "th"       
+        return str(number) + "th"  """      
+ 
+ 
+ 
     
 for i in range(102):   
     print(ordinalSuffix(i))
     
 assert ordinalSuffix(0) == '0th'
-
 assert ordinalSuffix(1) == '1st'
-
 assert ordinalSuffix(2) == '2nd'
-
 assert ordinalSuffix(3) == '3rd'
-
 assert ordinalSuffix(4) == '4th'
-
 assert ordinalSuffix(10) == '10th'
-
 assert ordinalSuffix(11) == '11th'
-
 assert ordinalSuffix(12) == '12th'
-
 assert ordinalSuffix(13) == '13th'
-
 assert ordinalSuffix(14) == '14th'
-
 assert ordinalSuffix(101) == '101st'
