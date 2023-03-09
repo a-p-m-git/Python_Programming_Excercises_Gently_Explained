282511c99b87ce1dfe4209d9c7b8997cd0732f76
apm <apm@matlock.ca>
2023-03-09 18:14:54 -0400

    exercise 7 complete

---
+++ 8 READ WRITE FILE/main.py
@@ -0,0 +1,23 @@
+def writeToFile(filename, text):
+    with open(filename, 'w') as f:
+        data = text
+        f.write(data)
+    
+
+def appendToFile(fileName, text):
+    with open(fileName, 'a') as f:
+        data = text
+        f.write(data)
+
+def readFromFile(filename):
+    with open(filename, 'r') as f:
+        return f.read()
+    
+    
+    
+
+
+#readFromFile('greet.txt')
+writeToFile('greet.txt', 'Hello!\n')
+appendToFile('greet.txt', 'Goodbye!\n')
+assert readFromFile('greet.txt') == 'Hello!\nGoodbye!\n'
